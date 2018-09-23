b2b7e856ccbbc45b5475da1c45e59fdd6fa0b25a
BenjaminBLi <li.benjamin.b@gmail.com>
2018-09-22 22:47:13 -0400

    Completed basic fragment for cameras

---
+++ app/src/main/java/me/yruili/yorku/wasteful/CameraFragment.kt
@@ -0,0 +1,20 @@
+package me.yruili.yorku.wasteful
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    companion object {
+        fun newInstance(): CameraFragment {
+            return CameraFragment()
+        }
+    }
+}
--- app/src/main/java/me/yruili/yorku/wasteful/MainActivity.kt
+++ app/src/main/java/me/yruili/yorku/wasteful/MainActivity.kt
@@ -2,12 +2,34 @@ package me.yruili.yorku.wasteful
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var viewPager = findViewById<ViewPager>(R.id.viewPager);
+
+        var adapterViewPager = MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapterViewPager);
+        viewPager.setCurrentItem(0);
+
+    }
+
+    class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return CameraFragment.newInstance()//some shit happens idfk;
+        }
+
+        override fun getCount(): Int {
+            return 1;
+        }
 
 
     }
